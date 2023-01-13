4a9fd99ceed6b39f6715ff4b9b2676a8ce43e985
ZlataEkert <zlataekert@gmail.com>
2023-01-13 16:45:56 +0200

    add app level state, feedback items

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Irure et ex anim occaecat est qui adipisicing reprehenderit do enim. Labore sunt quis proident dolore qui sunt enim cillum occaecat. Lorem cillum incididunt ullamco qui enim mollit pariatur voluptate minim ullamco mollit non.'
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Irure et ex anim occaecat est qui adipisicing reprehenderit do enim. Labore sunt quis proident dolore qui sunt enim cillum occaecat. Lorem cillum incididunt ullamco qui enim mollit pariatur voluptate minim ullamco mollit non.'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Irure et ex anim occaecat est qui adipisicing reprehenderit do enim. Labore sunt quis proident dolore qui sunt enim cillum occaecat. Lorem cillum incididunt ullamco qui enim mollit pariatur voluptate minim ullamco mollit non.'
+  },
+]
+
+export default FeedbackData
