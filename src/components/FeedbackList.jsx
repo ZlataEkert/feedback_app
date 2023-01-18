128c7b4dea7d66da726d1019df230a558a72f948
ZlataEkert <zlataekert@gmail.com>
2023-01-18 16:11:38 +0200

    add feedback to the global state

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,21 +2,23 @@ import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({ feedback, handleDelete }) {
-  if (!feedback || feedback.length === 0) {
+  if (!feedback || !feedback.length) {
     return <p>No Feedback Yet</p>
   }
 
-  return <div className='feedback-list'>
-    {feedback.map((item) => (
-      <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-    ))}
-  </div>
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={`${item.id}`} item={item} handleDelete={handleDelete}/>
+      ))}
+    </div>
+  )
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
