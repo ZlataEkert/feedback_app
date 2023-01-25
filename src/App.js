fd3b25d96baec36eee91a0fc8313ffbad8611baf
ZlataEkert <zlataekert@gmail.com>
2023-01-25 18:01:41 +0200

    create context and provider

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './components/pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 import FeedbackData from './data/FeedbackData'
 
 function App() {
@@ -23,24 +25,27 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className='container'>
-        <Routes>
-        <Route exact path='/' element={
-          <>
-          <FeedbackForm handleAdd={addFeedback} />
-          <FeedbackStats feedback={feedback} />
-          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-          </>
-        }>
-        </Route>
-      
-        <Route path='/about' element={<AboutPage />} />
-        </Routes>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+          <Route exact path='/' element={
+            <>
+            <FeedbackForm handleAdd={addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }>
+          </Route>
+        
+          <Route path='/about' element={<AboutPage />} />
+          </Routes>
 
-      </div>
-    </Router>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
