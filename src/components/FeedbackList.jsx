df5823b2f27283ae576796fa639780a440cb7b7c
ZlataEkert <zlataekert@gmail.com>
2023-02-17 14:42:16 +0200

    add feedaback & setting a proxy

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -7,29 +7,34 @@ import FeedbackContext from '../context/FeedbackContext'
 function FeedbackList() {
   const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if (!isLoading && (!feedback || !feedback.length)) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>
   }
 
-  return isLoading ? <Spinner /> : (<div className='feedback-list'>
-  <AnimatePresence>
-  {feedback.map((item) => (
-    <motion.div 
-      key={item.id}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <FeedbackItem key={`${item.id}`} item={item} />
-    </motion.div>
-  ))}
-  </AnimatePresence>
-</div>)
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  )
 
   // return (
   //   <div className='feedback-list'>
   //     {feedback.map((item) => (
-  //       <FeedbackItem key={`${item.id}`} item={item} handleDelete={handleDelete}/>
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
   //     ))}
   //   </div>
   // )
